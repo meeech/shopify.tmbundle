56a3cced14b43fa992b542dc576c6a4ff9b57bef
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-27 00:35:01 -0400

    pages download working

---
+++ Support/pages/download_all.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "<h2>Downloading all pages from<br>{$config->store}</h2>";
+
+$requestUrlTemp = 'http://%1$s:%2$s@%3$s/admin/pages.json';
+$requestUrl = sprintf($requestUrlTemp, $config->api_key, $config->password, $config->store);
+
+$response = json_decode(`curl --connect-timeout 20 -s -g '$requestUrl'`);
+
+if(!is_object($response) || !property_exists($response, 'pages')) {
+    echo "Error: Assets list not returned. Could just be a temporary error. Feel free to try again.<br>";
+    echo "Req Url: {$requestUrl}";
+    die();
+}
+
+$pages_path = TM_PROJECT_DIRECTORY.DIRECTORY_SEPARATOR.'pages';
+if(!file_exists($pages_path)) {
+    mkdir($pages_path);
+}
+
+foreach ($response->pages as $page) {
+    $page_file_name = $pages_path.DIRECTORY_SEPARATOR.$page->handle.'.html';
+    echo "Downloading {$page->title}<br>";
+    file_put_contents($page_file_name, $page->body_html);
+
+    //Here's where shit gets fun... just learnt about xattr
+    `xattr -w id "{$page->id}" {$page_file_name}`;
+}
+echo 'Done.';
