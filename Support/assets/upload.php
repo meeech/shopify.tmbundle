95352ebfa593aa1746e8c1316656c705ed82050d
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-06-14 02:11:44 -0400

    Switch UPLOAD ASSET command over to new structure. old behaviour working

---
+++ Support/assets/upload.php
@@ -0,0 +1,32 @@
+<?php
+define('TM_BUNDLE_SUPPORT',getenv('TM_BUNDLE_SUPPORT'));
+require_once TM_BUNDLE_SUPPORT.DIRECTORY_SEPARATOR.'vars.php';
+require_once TM_BUNDLE_SUPPORT.DIRECTORY_SEPARATOR.'functions.php';
+
+$assetKey = calc_asset_key(getenv('TM_FILEPATH')); 
+
+//if its an image file, throw an error message.
+if(in_array(pathinfo($assetKey, PATHINFO_EXTENSION), $imageExtensions)) {
+    echo "*Error: This is an image file. Use Send Selected Assets to Shopify instead.";
+    exit();
+}
+
+$filecontents = htmlentities(file_get_contents('php://stdin'), ENT_QUOTES, 'UTF-8');
+$reqData = sprintf($xmlDataTemp, $filecontents, $assetKey);
+
+//Dump the xml into a tmp file
+$xmlFile = tempnam('/tmp', 'foo').'.xml';
+file_put_contents($xmlFile, $reqData);
+
+$response = send_asset($api_key, $password, $store ,$xmlFile);
+
+if('200' == $response) {
+    echo "Uploaded {$assetKey} to {$config->current}.";
+} else {
+    // Not ideal, but it works. Problem (though not much of one ): 
+    // response on a fail will return the full curl page: ie, shopify 404 full html, + error code at the bottom
+    // Will robustify if it becomes an issue. 
+    echo "*Error: Could not upload {$assetKey} to {$config->current}." ;
+}
+//And clean up
+unlink($xmlFile);
